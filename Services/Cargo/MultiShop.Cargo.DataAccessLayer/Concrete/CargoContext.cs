40965babd0b00124ce2d01daa02957174d1cf9e3
onalsinem <130303664+onalsinem@users.noreply.github.com>
2024-12-03 11:48:29 +0300

    updatethree

---
+++ Services/Cargo/MultiShop.Cargo.DataAccessLayer/Concrete/CargoContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MultiShop.Cargo.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.Cargo.DataAccessLayer.Concrete
+{
+    public class CargoContext :DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                "Server=localhost,1441; Initial Catalog=CargoDb;User=sa; Password=123456aA*");
+        }
+
+        public DbSet<CargoCompany> CargoCompanys { get; set; }
+
+        public DbSet<CargoCustomer> CargoCustomers { get; set; }
+
+        public DbSet<CargoDetail> CargoDetails { get; set; }
+
+        public DbSet<CargoOperation> CargoOperations { get; set; }
+    }
+}
